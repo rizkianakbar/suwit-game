02ccd59e92e0e54cd9abe5ff95ca32caf94286cf
rizkianakbar <rizkianakbar4@gmail.com>
2021-03-31 13:45:57 +0700

    ADD : route for game page, hmm i forgot

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./assets/css/index.css";
 import SelectionGame from "./pages/SelectionGame";
 import SelectionRound from "./pages/SelectionRound";
 import PlayWithFriends from "./pages/PlayWithFriends";
+import PlayWithComputer from "./pages/PlayWithComputer";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={SelectionGame} exact path="/" />
-        <Route component={SelectionRound} path="/round" />
-        <Route component={PlayWithFriends} path="/comingsoon" />
+        <Route component={SelectionRound}  path="/round" />
+        <Route component={PlayWithFriends}  path="/comingsoon" />
+        <Route component={PlayWithComputer}  path="/game" />
       </Switch>
     </Router>
   );
