1de97d9b9ab2a6bdfa301bf7e8a4cec2410524fb
rizkianakbar <rizkianakbar4@gmail.com>
2021-03-31 11:37:51 +0700

    ADD : coming soon page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./assets/css/index.css";
 import SelectionGame from "./pages/SelectionGame";
+import PlayWithFriends from "./pages/PlayWithFriends";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route component={SelectionGame} exact path="/" />
+        <Route component={PlayWithFriends}  path="/comingsoon" />
+
       </Switch>
     </Router>
   );
---
+++ src/pages/PlayWithFriends.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlayWithFriends() {
+  return (
+    <>
+        <div className="title mt-9">Coming Soon ... </div>
+    </>
+  );
+}
+
+export default PlayWithFriends;
